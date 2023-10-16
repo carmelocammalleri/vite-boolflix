32321d3d92326bc6d89b91d2bc8ac026e0fc5e1b
Melozzo <carmecamma1995@gmail.com>
2023-10-16 15:29:07 +0200

    [create]: input text and select/options

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,8 +5,9 @@
 <template>
   <input type="text" placeholder="Cerca il titolo...">
   <select name="selectionType" id="">
-    
-    
+    <option value="film">film</option>
+    <option value="serie">serie</option>
+    <option value="tutto">tutto</option>
   </select>
 
 </template>
