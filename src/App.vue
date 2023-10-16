38c7997eba7e1b5ca0a55bd8fd8ea722fac19ca3
Melozzo <carmecamma1995@gmail.com>
2023-10-16 15:47:37 +0200

    [create&import]: store.js

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
   import Header from './components/Header.vue'
   import Main from './components/Main.vue'
+  import { store } from './data/store';
 
   export default {
     name: 'App',
---
+++ src/data/store.js
@@ -0,0 +1,4 @@
+import {reactive} from 'vue'
+export const store = reactive ({
+  apiUrl:'https://developers.themoviedb.org/3/search/movie?api_key=ed90eed6f9f0d435c220a5cac787b79e'
+})
