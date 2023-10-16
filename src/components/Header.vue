dce3e8426cd4a6aa7d1e9d12a7fc600a88a619fd
Melozzo <carmecamma1995@gmail.com>
2023-10-16 15:18:44 +0200

    [install]: axios, bootstrap, scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script>
 
 </script>
 
@@ -7,6 +7,6 @@
 
 </template>
 
-<style scoped>
+<style>
 
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,12 @@
+<script>
+  export
+</script>
+
+<template>
+
+
+</template>
+
+<style>
+
+</style>
