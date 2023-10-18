73380a4d05711cee40ab84353d735ab3003e93ea
Melozzo <carmecamma1995@gmail.com>
2023-10-18 18:51:30 +0200

    [set]: image in cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -27,18 +27,18 @@ export default{
 
 <template>
 
-<div class="container">
+<div class="container containerWrapper">
   <h1>{{title}}</h1>
-  <div class="row row-cols-4">
+  <div class="row row-cols-5">
 
     <!-- Card contenente i vari film -->
-    <div class="card my-2" 
+    <div class="card my-1" 
         v-for="film in store[type]" 
         :key="film.id">
-        <div class="card-body">
+        <div class="card-body d-none">
 
           <!-- lingua inserita -->
-          <h3 class="card-title">{{ film.title || film.name }}</h3>
+          <h4 class="card-title">{{ film.title || film.name }}</h4>
           <p class="card-title text-decoration-underline ">{{film.original_title || film.original_name }}</p>
   
           <div v-if="flags.includes(film.original_language)">
@@ -55,22 +55,23 @@ export default{
           <!-- /lingua inserita -->
           
         </div>
-        <img 
-          class="imagePoster" 
-          :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" 
-          alt="{{ film.poster_path }}"
-          v-if="film.poster_path">
-          <img v-else src="https://i.pinimg.com/736x/47/21/d6/4721d63c03ee90875e960572d8a676f8.jpg" alt="">
+        <div class="posters">
+          <img 
+            class="imagePoster" 
+            :src="`https://image.tmdb.org/t/p/w342/${film.poster_path}`" 
+            alt="{{ film.poster_path }}"
+            v-if="film.poster_path">
+            <img 
+              class="imagePoster" 
+              v-else src="https://i.pinimg.com/736x/47/21/d6/4721d63c03ee90875e960572d8a676f8.jpg" alt="">
+        </div>
     </div>
   </div>
 </div>
 
 </template>
 
-<style lang="scss" scoped>
-  .card .flag{
-    width: 30px;
-    margin-right: 5px;
-  }
+<style lang="scss">
+  @use '../scss/partials/card.scss';
 
 </style>
